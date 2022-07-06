d1d40a11ab9ad90a0039f29e1ceb7977adf53b25
Josel Catalan <catalanjosel123@gmail.com>
2022-07-06 11:22:24 +0800

    create server js

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+// Import modules
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
